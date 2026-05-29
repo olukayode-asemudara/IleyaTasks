215854f9d789d6251c07a20ee0b697803e7a91e2
Olukayode <theolukayodeasemudara@gmail.com>
2026-05-29 16:50:05 +0100

    generate randomBooks and page number works fine, as well as add to books array... but new bug introduced

--- dayTwo/BookSuggestionSystem.java
+++ dayTwo/BookSuggestionSystem.java
@@ -2,76 +2,89 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BookSuggestionSystem {
+
+    static ArrayList<String> books = new ArrayList<>();
     
-    public static String generateRandomBook(String[] books) {
+    public static String generateRandomBook() {
     
-        java.util.Random randomBookGenerator = new java.util.Random();
+        java.util.Random randomGenerator = new java.util.Random();
     
-        int randomIndex = randomBookGenerator.nextInt(books.length);
-        int randomPage = randomBookGenerator.nextInt(100);
+        int randomIndex = randomGenerator.nextInt(books.size());
+        int randomPage = randomGenerator.nextInt(100);
     
-        String randomBook = books[randomIndex] + ", page: " + randomPage;
+        String randomBook = books.get(randomIndex) + ", page: " + randomPage;
         
-        // System.out.println(randomBook);
+        System.out.println(randomBook);
         
         return randomBook;
     }
     
     
-    public static void addToBooksArray(String bookName){
-        ArrayList<String> books = new ArrayList<>();
-        books.add(bookName);
-    }
-
-    public static void main(String[] args) {
-        Scanner inputCollector = new Scanner(System.in);
-    
-        // books array collection
-        ArrayList<String> books = new ArrayList<>();
-                   
-                
+    public static void addToBooksArray(String bookName) {
         
-        while(true){
+        books.add(bookName);
+    
+        System.out.println(bookName + " added successfully!");
+    }
+    
+    public static void getAllBooks(){
+        
+    }
+    
+    
+    public static void main(String[] args){
+        java.util.Scanner inputCollector = new java.util.Scanner(System.in);
+        
         
         System.out.println("""
-                
-    Welcome to the Book Suggestion System!
-    
-        1. Get Suggestions
-        2. Add Book
-        3. Remove Book
-        4. Update book
-        5. Show books
-    
-    Select option to begin
-                """);
-                
-        int option = inputCollector.nextInt();
-        inputCollector.nextLine();
-
+                    
+        Welcome to the Book Suggestion System!
+        
+            1. Get Suggestions
+            2. Add Book
+            3. Remove Book
+            4. Update book
+            5. Show books
+        
+        Select option to begin
+                    """);
+                    
+        while(true){
+        
+            int day = inputCollector.nextInt();
             
-            switch(option){
+            switch(day){
             
                 case 1:
-                    System.out.println(generateRandomBook(books));
-                    System.out.print("Would you like to get another suggestion? (yes/no): ");
-                    String select = inputCollector.nextLine();
+                if(books.isEmpty()) {
+                    System.out.println("No books added yet.");
+                }
+                else {
+                    System.out.println(generateRandomBook());
+                }
+                break;
                     
-                        if(select.toLowerCase().equals("yes")){
-                             System.out.println(generateRandomBook(books));
-                        } else{
-                            System.out.println("exited!!!");
-                            break;
-                        }
-                        
                 case 2:
-                    System.out.println("enter book title");
-                    String bookTitle = inputCollector.nextLine();
-                    addToBooksArray(books, bookTitle);
+                    System.out.println("Enter book name:");
+                    String bookName = inputCollector.nextLine();
+                    addToBooksArray(bookName);
+
+                    break;
                     
+                case 3:
+                    System.out.println("Update Books");
+                    break;
                     
+                case 4:
+                    System.out.println("Show all Books");
+                    break;
+                    
+                default:
+                    System.out.println("Invalid Entry");
+                    break;
             }
         }
+
 
     }
 }
